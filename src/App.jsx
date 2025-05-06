c145324ab13477575fbf8609d688fe870558f9b7
guptaRishi00 <rishigupta54066@gmail.com>
2025-05-06 21:40:15 +0530

    changed font in benefit cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import CryptoSwap from "./components/CryptoSwap";
-import { WalletConnectionProvider } from "./solana/WalletProvider";
 
 function App() {
   return (
-    <WalletConnectionProvider>
+    <>
       <Header />
       <CryptoSwap />
-    </WalletConnectionProvider>
+    </>
   );
 }
 
